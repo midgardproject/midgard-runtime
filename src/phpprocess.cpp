c5b8798ce0c2a8974cb17f14a6ffc82f0ea71b2e
Michael Dominic K <mdk@mdk.am>
2009-12-02 17:23:37 +0000

    Php process now fired with two env vars with user name and user real name.
    Env vars:
    	MIDGARD_ENV_USER_NAME
    	MIDGARD_ENV_REAL_NAME

--- src/phpprocess.cpp
+++ src/phpprocess.cpp
@@ -1,5 +1,6 @@
 #include "phpprocess.h"
 #include <QDebug>
+#include <pwd.h>
 
 PhpProcess::PhpProcess(QDir baseDir, QObject *parent) : RuntimeProcess(baseDir, parent)
 {
@@ -13,8 +14,24 @@ void PhpProcess::start()
     arguments << "-b";
     arguments << "127.0.0.1:9000";
 
+    // Standard env
     env << ("MIDGARD_ENV_GLOBAL_CACHEDIR=" + baseDir.absoluteFilePath("cache"));
     env << ("MIDGARD_ENV_GLOBAL_SHAREDIR=" + baseDir.absoluteFilePath("share"));
+
+    // Get the user name and the real name
+    struct passwd *userinfo = getpwuid(getuid());
+    QString username = QString();
+    QString realname = QString();
+
+    if (userinfo->pw_name != NULL)
+        username = QString(userinfo->pw_name);
+
+    if (userinfo->pw_gecos != NULL && QString(userinfo->pw_gecos).split(',').size() > 0)
+        realname = QString(userinfo->pw_gecos).split(',').at(0);
+
+    env << ("MIDGARD_ENV_USER_NAME=" + username);
+    env << ("MIDGARD_ENV_REAL_NAME=" + realname);
+
     setEnvironment(env);
     QProcess::start("php5-cgi", arguments);
 }
